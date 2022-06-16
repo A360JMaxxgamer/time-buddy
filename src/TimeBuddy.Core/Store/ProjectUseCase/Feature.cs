12b7c64a732da616c502c3bc2ed35c724f0cbbcf
Marco Hamann <marcohamann89@hotmail.de>
2022-06-16 12:10:54 +0200

    add empty ProjectPage

---
+++ src/TimeBuddy.Core/Store/ProjectUseCase/Feature.cs
@@ -0,0 +1,12 @@
+﻿using Fluxor;
+
+namespace TimeBuddy.Core.Store.ProjectUseCase;
+
+public class Feature : Feature<ProjectState>
+{
+    /// <inheritdoc />
+    public override string GetName() => nameof(ProjectState);
+
+    /// <inheritdoc />
+    protected override ProjectState GetInitialState() => new ProjectState(null, false);
+}
