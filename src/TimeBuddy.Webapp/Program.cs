d33b4e62f316db9ad7743a20a115a8c519b51702
Marco Hamann <marcohamann89@hotmail.de>
2022-08-08 13:58:48 +0200

    enable authentication on webapp

--- src/TimeBuddy.Webapp/Program.cs
+++ src/TimeBuddy.Webapp/Program.cs
@@ -22,6 +22,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
 builder.Services
+    .AddOidcAuthentication(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options.ProviderOptions);
+    });
+
+builder.Services
     .AddLogging()
     .AddMudServices()
     .AddTimeBuddyServices()
